8f63083e03cfe430317d690e486d2fe63b23d4fc
Paul <paula.farid9@gmail.com>
2025-09-23 00:03:24 +0300

    second

--- src/main/java/Utiles/CommonHelper/RandomSource.java
+++ src/main/java/Utiles/CommonHelper/RandomSource.java
@@ -1,4 +1,52 @@
 package Utiles.CommonHelper;
 
+import com.github.javafaker.Faker;
+import com.github.javafaker.PhoneNumber;
+
+import java.util.Random;
+
 public class RandomSource {
+     static  Faker faker=new Faker();
+     static  Random random = new Random();
+
+    //Methods
+    public static String createFullName(){
+        return faker.name().fullName();
+    }
+    public static String createEmail(){
+        return faker.internet().emailAddress();
+    }
+    public static String createPassword(){
+        return faker.internet().password();
+    }
+    public static String createFirstName(){
+        return faker.name().firstName();
+    }
+    public static String createLastName(){
+        return faker.name().lastName();
+    }
+    public static String createAddress(){
+        return faker.address().streetAddress();
+    }
+    public static String createState(){
+        return faker.address().state();
+    }
+    public static String createCity(){
+        return faker.address().city();
+    }
+    public static String createZipCode(){
+        return faker.address().zipCode();
+    }
+    public static String createEgyptianMobileNumber(){
+
+        String[] prefixes = {"010", "011", "012", "015"};
+
+        String prefix = prefixes[random.nextInt(prefixes.length)];
+
+        String remainingDigits = faker.number().digits(8);
+
+        return prefix + remainingDigits;
+    }
+
+
 }
